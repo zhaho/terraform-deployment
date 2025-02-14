4653fefd1106066600d58971295b681f71c2ff42
kenneth ros <zhaho.ros@gmail.com>
2025-02-14 16:55:17 +0100

    working looping servers

--- environments/lab-servers/main.tf
+++ environments/lab-servers/main.tf
@@ -1,14 +1,16 @@
-module "lab_vm" {
+module "lab_vms" {
   source = "../../modules/proxmox-vm"
 
+  for_each = { for vm in var.vms : vm.name => vm }
+
   target_node       = var.target_node
-  vm_name           = var.vm_name
-  vm_cores          = var.vm_cores
-  vm_memory         = var.vm_memory
-  vm_disk_size      = var.vm_disk_size
+  vm_name           = each.value.name
+  vm_cores          = each.value.cores
+  vm_memory         = each.value.memory
+  vm_disk_size      = each.value.disk_size
   vm_network_bridge = var.vm_network_bridge
-  vm_static_ip      = var.vm_static_ip
-  vm_gateway        = var.vm_gateway
+  vm_static_ip      = each.value.static_ip
+  vm_gateway        = each.value.gateway
   vm_datastore      = "local-lvm"
   template_id       = 9000  # ID of the template VM
 }
--- environments/lab-servers/variables.tf
+++ environments/lab-servers/variables.tf
@@ -7,6 +7,44 @@ variable "vm_network_bridge" { default = "vmbr0" }
 variable "vm_static_ip" { default = "10.4.5.30/24" }
 variable "vm_gateway" { default = "10.4.5.1" }
 
+variable "vms" {
+  description = "List of VM configurations"
+  type = list(object({
+    name           = string
+    cores          = number
+    memory         = number
+    disk_size      = number
+    static_ip      = string
+    gateway        = string
+  }))
+  default = [
+    {
+      name      = "lab-vm-1"
+      cores     = 2
+      memory    = 4096
+      disk_size = 20
+      static_ip = "10.4.5.31/24"
+      gateway   = "10.4.5.1"
+    },
+    {
+      name      = "lab-vm-2"
+      cores     = 2
+      memory    = 4096
+      disk_size = 20
+      static_ip = "10.4.5.32/24"
+      gateway   = "10.4.5.1"
+    },
+    {
+      name      = "lab-vm-3"
+      cores     = 2
+      memory    = 4096
+      disk_size = 20
+      static_ip = "10.4.5.33/24"
+      gateway   = "10.4.5.1"
+    }
+  ]
+}
+
 
 variable "pm_api_url" {
   description = "Proxmox API URL"
@@ -23,3 +61,4 @@ variable "pm_api_token_secret" {
   type        = string
   sensitive   = true
 }
+
