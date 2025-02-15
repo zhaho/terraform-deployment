bbfef226a8321ad39bbbcfc66150dd0506245518
kenneth ros <zhaho.ros@gmail.com>
2025-02-15 13:31:33 +0100

    fix: taskfile lullull

--- environments/lab-servers/variables.tf
+++ environments/lab-servers/variables.tf
@@ -1,11 +1,5 @@
 variable "target_node" { default = "proxmox" }
-variable "vm_name" { default = "lab-vm" }
-variable "vm_cores" { default = 2 }
-variable "vm_memory" { default = 4096 }
-variable "vm_disk_size" { default = 20 }
 variable "vm_network_bridge" { default = "vmbr0" }
-variable "vm_static_ip" { default = "10.4.5.30/24" }
-variable "vm_gateway" { default = "10.4.5.1" }
 
 variable "vms" {
   description = "List of VM configurations"
