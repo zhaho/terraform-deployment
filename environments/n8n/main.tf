cb62590b4f6840c0d6b9dd889226d09ce17805fd
kenneth <zhaho.ros@gmail.com>
2025-07-26 15:05:49 +0000

    added n8n

---
+++ environments/n8n/main.tf
@@ -0,0 +1,16 @@
+module "vms" {
+  source = "../../modules/proxmox-vm"
+
+  for_each = { for vm in var.vms : vm.name => vm }
+
+  target_node       = var.target_node
+  vm_name           = each.value.name
+  vm_cores          = each.value.cores
+  vm_memory         = each.value.memory
+  vm_disk_size      = each.value.disk_size
+  vm_network_bridge = var.vm_network_bridge
+  vm_static_ip      = each.value.static_ip
+  vm_gateway        = each.value.gateway
+  vm_datastore      = "local-lvm"
+  template_id       = 9200  # ID of the template VM
+}
---
+++ environments/n8n/provision.tf
@@ -0,0 +1,19 @@
+resource "null_resource" "remote_provision" {
+  for_each = { for vm in var.vms : vm.name => vm }
+
+  depends_on = [module.vms]
+
+  connection {
+    type        = "ssh"
+    user        = var.host_user
+    private_key = file("~/.ssh/id_rsa")
+    host        = trimsuffix(each.value.static_ip, "/24")
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "cloud-init status --wait",  
+      "ansible-pull -U https://github.com/zhaho/ansible-deployment.git -e host_user=${var.host_user} zsh.yml -vvv"
+    ]
+  }
+}
